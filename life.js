8299ffbd90ee2ed58f438a064076aca1fc4d3d46
Cyril <105249525+cyrilc0de@users.noreply.github.com>
2022-06-23 23:12:49 +0100

    Added file on my data

---
+++ life.js
@@ -0,0 +1,15 @@
+let firstName = "Cyril" ;
+let middleName= "sylvester";
+let lastName  = "Ekpenyong" ;
+let height= "1.5";
+let country ="Nigeria";
+
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+console.log(height);
+console.log(country);
+
+
+alert("I am cyril Sylvester Ekpenyong,with 1.57 height and  I am a Nigerian")
+       
